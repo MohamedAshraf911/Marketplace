8c8621cf39bdce3946442bb9a738e743b458ae98
al719 <88440404+al719@users.noreply.github.com>
2023-05-17 18:50:00 +0300

    Update views.py

--- item/views.py
+++ item/views.py
@@ -5,7 +5,49 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import EditItemForm, NewItemForm
 from .models import Category, Item
+# /////////////////////////////////////////////////
+from django.http import JsonResponse
+from .models import Item
+from .serializers import Dserializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+# /////////////////////////////////////////////////
 
+
+@api_view(['GET', 'POST'])
+def Item_list(request ,format=None):
+    if request.method == 'GET':
+        items = Item.objects.all()
+        serializer = Dserializer(items, many=True)
+        #return JsonResponse({'items':serializer.data}, safe=False)
+        return Response(serializer.data) 
+    if request.method == 'POST':
+        serializer = Dserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+
+
+@api_view(['GET','PUT','DELETE'])
+def item_detail(request,id,format = None):
+    try:
+       item = Item.objects.get(pk = id)
+    except Item.DoesNotExist:
+        Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = Dserializer(item)
+        return Response(serializer.data) 
+    if request.method == 'PUT':
+        serializer = Dserializer(item,data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
+# /////////////////////////////////////////////////
 def items(request):
     query = request.GET.get('query','')
     category_id = request.GET.get('category',0)
