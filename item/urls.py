e6cd0960300b635de1a67db5fea22c294a404286
al719 <88440404+al719@users.noreply.github.com>
2023-05-17 18:50:59 +0300

    Update urls.py

--- item/urls.py
+++ item/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/edit/', views.edit, name='edit'),
+    path('jitems/', views.Item_list),
+    path('jitems/<int:id>', views.item_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
